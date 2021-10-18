5a82114b94c2d14a53637a02b3d79d56375ca255
Yannick B <yannickbiheul@gmail.com>
2021-10-18 14:42:43 +0200

    Ajout hashage mot de passe utilisateur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,23 +4,51 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
+    private $manager;
+
+    public function __construct(EntityManagerInterface $manager) {
+        $this->manager = $manager;
+    }
     /**
      * @Route("/register", name="security_register")
      */
-    public function register(): Response
+    public function register(Request $request, UserPasswordHasherInterface $encoder): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+
+        // Analyse de la requête par le formulaire
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Traitement des données reçues du formulaire
+            $password_hash = $encoder->hashPassword($user, $user->getPassword());
+            $user->setPassword($password_hash);
+            $this->manager->persist($user);
+            $this->manager->flush();
+            return $this->redirectToRoute("security_login");
+        }
 
         return $this->render('security/index.html.twig', [
             'controller_name' => 'Inscription',
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login(): Response
+    {
+        return $this->render('security/login.html.twig');
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,8 +17,8 @@ class RegisterType extends AbstractType
             ->add('firstname')
             ->add('lastname')
             ->add('email')
-            ->add('password')
-            ->add('passwordConfirm')
+            ->add('password', PasswordType::class)
+            ->add('passwordConfirm', PasswordType::class)
         ;
     }
 
